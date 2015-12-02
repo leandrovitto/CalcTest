f4632ddd09d57684159b1250963dfc29f6a431bb
leandrovitto <vitto.leandro@gmail.com>
2015-12-02 23:15:03 +0100

    r

--- daogenerator/src/main/java/com/example/MyDaoGenerator.java
+++ daogenerator/src/main/java/com/example/MyDaoGenerator.java
@@ -1,4 +1,35 @@
 package com.example;
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
 
 public class MyDaoGenerator {
+    public static void main(String args[]) throws Exception {
+        Schema schema = new Schema(1, "com.example.daypilot.sqlite");
+
+        Entity rooms = schema.addEntity("Rooms");
+        rooms.addIdProperty();
+        rooms.addStringProperty("name");
+        rooms.addStringProperty("capacity");
+        rooms.addStringProperty("status");
+        rooms.addStringProperty("note");
+
+        Entity reservations = schema.addEntity("Reservations");
+        reservations.addIdProperty();
+        reservations.addStringProperty("name");
+        reservations.addLongProperty("start");
+        reservations.addLongProperty("end");
+        reservations.addLongProperty("room_id");
+
+
+        Property roomsId = reservations.addLongProperty("roomsId").getProperty();
+        reservations.addToOne(rooms, roomsId);
+
+       ToMany roomToReservation = rooms.addToMany(reservations, roomsId);
+        roomToReservation.setName("reservations");
+
+        new DaoGenerator().generateAll(schema, "../app/src/main/java");
+    }
 }
